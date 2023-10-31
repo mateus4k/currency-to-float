72371a69a0e20cda4cfbbae74d5eafa14723e8a1
Mateus Sampaio <mateus4k@protonmail.ch>
2023-10-31 16:03:49 -0300

    refactor(parser): extract regex

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,35 +1,16 @@
-import {parse} from '../src';
+import lib, { parse } from '../src';
+import { inputs } from './inputs';
 
-test.each([
-  ['$12.50 USD', 12.5],
-  ['$12 USD', 12],
-  ['$12.50 CAD', 12.5],
-  ['12,50 $ CAD', 12.5],
-  ['$12.50 AUD', 12.5],
-  ['12,50 € EUR', 12.5],
-  ['12.50 € EUR', 12.5],
-  ['12.50€', 12.5],
-  ['€12.50', 12.5],
-  ['12.500,- €', 12500],
-  ['12,- €', 12],
-  ['€12,50 EUR', 12.5],
-  ['€12.50 EUR', 12.5],
-  ['£12.50 GBP', 12.5],
-  ['£12,50 GBP', 12.5],
-  ['¥1250 JPY', 1250],
-  ['¥109,133 JPY', 109.133],
-  ['¥54.566,8 JPY', 54566.8],
-  ['$12.50 NZD', 12.5],
-  ['$12.50 HKD', 12.5],
-  ['$12.50 SGD', 12.5],
-  ['12,50 kr. DKK', 12.5],
-  ['14 £', 14],
-  ['R$ 12,50', 12.5],
-  ['R$ 12.50', 12.5],
-  ['$ 1.200,50', 1200.5],
-  ['$ 1,200.50', 1200.5],
-  ['$123,456,789,00', 123456789],
-])('parse("%s") === %f', (input, expected) => {
-  const parsed = parse(input);
-  expect(parsed).toBe(expected);
+describe('custom export', () => {
+  it.each(inputs)('parse("%s") === %f', ({ input, output }) => {
+    const parsed = parse(input);
+    expect(parsed).toBe(output);
+  });
+});
+
+describe('export default', () => {
+  it.each(inputs)('parse("%s") === %f', ({ input, output }) => {
+    const parsed = lib(input);
+    expect(parsed).toBe(output);
+  });
 });
